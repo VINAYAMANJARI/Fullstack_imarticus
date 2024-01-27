a9c8d1b677ebae52f9eba5d07d975bfab52d2570
VINAYAMANJARI <vinayakrishnan27@gmail.com>
2024-01-27 10:46:59 +0530

    initial commit

---
+++ Jan 26/script.js
@@ -0,0 +1,21 @@
+var Price = 100000;
+        var Product ='Laptop';
+        var tax = 10;
+        console.log(Product)
+        var total;
+        total = Price + tax;
+        console.log(total);
+
+function simple()
+{
+    if(true)
+    {
+        console.log("Thank you for your help");
+    }
+}
+simple()
+
+var x = (x,y) => console.log(x*y);
+x(10,20)
+
+
